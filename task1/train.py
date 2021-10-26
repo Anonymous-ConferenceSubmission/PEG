1a1440d893da2052ea06855abafaac3dc4dd0e4b
Anonymous-ConferenceSubmission <92772402+Anonymous-ConferenceSubmission@users.noreply.github.com>
2021-10-26 20:25:37 +0800

    Update train.py
    fix a small bug

--- task1/train.py
+++ task1/train.py
@@ -237,6 +237,8 @@ def train_model_plus(model, optimizer, x, edge_index, id_train_positive, id_trai
                 #loss_history.append(loss_train)
                 loss_train.backward()
                 optimizer.step()
+                with torch.no_grad():
+                    model.fc.weight[0][0].clamp_(1e-5,100)
 
                 label_ids = label.to('cpu').numpy()
                 y_label_train = y_label_train + label_ids.flatten().tolist()
